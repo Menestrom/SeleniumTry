507baab208e68b66f95585a0f577a28259112b6f
Menestrom <42383096+Menestrom@users.noreply.github.com>
2018-10-17 18:22:47 +0300

    Update Bingo.cs

--- Bingo.cs
+++ Bingo.cs
@@ -1,47 +1,24 @@
-using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using OpenQA.Selenium.Interactions;
 
-
-namespace Selenium_try
+namespace PromotionPlanTests
 {
-    class BrowserTests
+    public static class Driver
     {
-        IWebDriver driver;
-        [SetUp]
-        public void startBrowser()
-        {
-            driver = new ChromeDriver("C:\\Users\\yevhenta\\.nuget\\packages\\Selenium.WebDriver.ChromeDriver\\2.41.0\\driver\\win32");
-        }
-        [Test]
-        public void test()
-        {
-            driver.Url = "https://m-guccibingo-qa3.test.bingosys.net/#/Login";
-            // driver.Manage().Window.Maximize();
-            IWebElement emailTextBox = driver.FindElement(By.Id("user-name"));
-            IWebElement passwordTextBox = driver.FindElement(By.Id("password"));
-            IWebElement singUpButton = driver.FindElement(By.Id("login-button"));
-
-            var actions = new Actions(driver);
-            actions.MoveToElement(emailTextBox);
-            emailTextBox.SendKeys("CashierTest1");
-            actions.MoveToElement(passwordTextBox);
-            passwordTextBox.SendKeys("Qwerty1");
-            singUpButton.Click();
-
-        }
-        [TearDown]
-        public void closeBrowser()
-        {
-            driver.Close();
-        }
-        
+        public static IWebDriver driver = new ChromeDriver();
     }
-}
+    public static class WebDriverExtensions
+    {
+        public static IWebElement FindElement(this IWebDriver driver, By by, int timeoutInSeconds)
+        {
+            if (timeoutInSeconds > 0)
+            {
+                var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeoutInSeconds)); 
+                return wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(by));
+            }
+            return driver.FindElement(by);
+        }
+    }
+
